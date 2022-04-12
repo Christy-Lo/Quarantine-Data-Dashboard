3c409ef00d5184e455ab5abc1683b26b1b7adc13
Christy-Lo <61792992+Christy-Lo@users.noreply.github.com>
2022-04-12 15:28:23 +0800

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-zm(ucxbc@*r=1g+cu*^r&m^cybv97#fusbha$%gnf4=(3)uxgx'
 SECRET_KEY = env.str('QDD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
